fe0efb18a3f3ac7dbb4e7161297062fd780ca72e
hanan-3m3 <hananmariyam2004@gmail.com>
2025-02-19 12:36:57 +0530

    Create 2.16 Persons.jsx

---
+++ 2.16 Persons.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+const Persons = ({ persons, handleDelete }) => {
+  return (
+    <ul>
+      {persons.map((person) => (
+        <li key={person.id}>
+          {person.name} {person.number} 
+          <button onClick={() => handleDelete(person.id, person.name)}>delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+Persons.propTypes = {
+  persons: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  handleDelete: PropTypes.func.isRequired,
+};
+
+export default Persons;
