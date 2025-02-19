7de96945f2cc7201486d033fe8654bb403626fea
hanan-3m3 <hananmariyam2004@gmail.com>
2025-02-19 12:25:46 +0530

    Create 2.13 persons.jsx

---
+++ 2.13 persons.jsx
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+
+const Persons = ({ persons, filter }) => {
+  return (
+    <ul>
+      {persons
+        .filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        .map((person) => (
+          <li key={person.id}>
+            {person.name} {person.number}
+          </li>
+        ))}
+    </ul>
+  );
+};
+
+// ✅ Define PropTypes to remove the ESLint warning
+Persons.propTypes = {
+  persons: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  filter: PropTypes.string.isRequired,
+};
+
+export default Persons;
