49491fccc57b4f060ac692886467331520a36705
hanan-3m3 <hananmariyam2004@gmail.com>
2025-02-19 12:32:15 +0530

    Create 2.15persons.jsx

---
+++ 2.15persons.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = () => axios.get(baseUrl).then((res) => res.data);
+const create = (newObject) => axios.post(baseUrl, newObject).then((res) => res.data);
+const update = (id, updatedObject) => axios.put(`${baseUrl}/${id}`, updatedObject).then((res) => res.data);
+const deletePerson = (id) => axios.delete(`${baseUrl}/${id}`);
+
+export default { getAll, create, update, deletePerson };
